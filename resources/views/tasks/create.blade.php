cafcb4cf4ef8d19f058a74700933423c4e216e62
Marcus Loo <marcuslooek@gmail.com>
2023-02-14 21:14:08 +0800

    fix

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,7 +9,7 @@
         @csrf
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <input type="text" name="name" placeholder="Task Name" class="rounded w-full mb-5" required />
+                <input type="text" name="name" placeholder="Task Name" class="rounded w-full mb-5" value="{{ old('name') ?? '' }}" required />
                 <div>
                     <a href="{{ route('tasks.index') }}" class="p-3 mr-5 text-gray-900 dark:text-gray-100 bg-gray-500 rounded">Cancel</a>
                     <button type="submit" class="p-3 text-gray-900 dark:text-gray-100 bg-green-500 rounded">Create</button>
